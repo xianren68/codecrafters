0714d175f61c97f38d65fbe808ecc98ff649c16f
xianren <xianren711@qq.com>
2025-01-06 22:24:46 +0800

    update

--- shell/rust/src/main.rs
+++ shell/rust/src/main.rs
@@ -1,9 +1,22 @@
+use ::std::fmt::Write as FmtWrite;
 use std::collections::HashMap;
-use std::{env, process};
-use std::sync::OnceLock;
-use::std::fmt::Write as FmtWrite;
-use std::io::{self, BufReader, BufWriter, BufRead, Write};
+use std::io::{self, BufRead, BufReader, BufWriter, Write};
 use std::path::Path;
+use std::sync::OnceLock;
+use std::{env, fs, process};
+
+#[cfg(target_os = "linux")]
+use std::os::unix::fs::PermissionsExt;
+#[cfg(target_os = "windows")]
+mod system {
+    pub const SPLIT_SYMBOL: &'static str = ";";
+    pub const SUFFIX: &'static str = ".exe";
+}
+#[cfg(target_os = "linux")]
+mod system {
+    pub const SPLIT_SYMBOL: &'static str = ":";
+    pub const SUFFIX: &'static str = "";
+}
 
 type Handler = fn(&[String]) -> Result<String, String>;
 static GLOBAL_MAP: OnceLock<HashMap<String, Handler>> = OnceLock::new();
@@ -14,7 +27,7 @@ struct RedirectOpt {
 }
 fn init_map() -> &'static HashMap<String, Handler> {
     GLOBAL_MAP.get_or_init(|| {
-        let mut map:HashMap<String, Handler> = HashMap::new();
+        let mut map: HashMap<String, Handler> = HashMap::new();
         map.insert("exit".to_string(), handle_exit);
         map.insert("echo".to_string(), handle_echo);
         map.insert("type".to_string(), handle_type);
@@ -37,19 +50,23 @@ fn main() {
             line.pop().unwrap();
         }
         match parse_args(line) {
-            Ok(args) => {
+            Ok(res) => {
+                let args = res.0;
+                let _ = res.1;
                 if args.is_empty() {
                     continue;
                 }
                 if map.contains_key(&args[0]) {
                     let handler = map.get(&args[0]).unwrap();
                     let val = handler(&args[1..]).unwrap();
-                    writer.write_fmt(format_args!("{}",val)).unwrap();
-                }else {
-                    writer.write_fmt(format_args!("{}: command not found\n", args[0])).unwrap();
+                    writer.write_fmt(format_args!("{}", val)).unwrap();
+                } else {
+                    writer
+                        .write_fmt(format_args!("{}: command not found\n", args[0]))
+                        .unwrap();
                     writer.flush().unwrap();
                 }
-            },
+            }
             Err(err) => {
                 writer.write_fmt(format_args!("{}\n", err)).unwrap();
             }
@@ -83,8 +100,14 @@ fn handle_type(args: &[String]) -> Result<String, String> {
     let mut res = String::new();
     for arg in args {
         if map.contains_key(arg) {
-            res.write_str(&*(arg.to_owned() + " is shell builtin\n")).unwrap();
+            res.write_str(&*(arg.to_owned() + " is shell builtin\n"))
+                .unwrap();
         } else {
+            let (flag, complete_path) = is_executable(arg);
+            if flag {
+                res.write_str(&*(arg.to_owned() + " is " + &*complete_path + "\n")).unwrap();
+                continue;
+            }
             res.write_str(&*(arg.to_owned() + ": not found\n")).unwrap();
         }
     }
@@ -93,12 +116,8 @@ fn handle_type(args: &[String]) -> Result<String, String> {
 
 fn handle_pwd(args: &[String]) -> Result<String, String> {
     match env::current_dir() {
-        Ok(dir) => {
-            Ok(dir.into_os_string().into_string().unwrap() + "\n")
-        },
-        Err(err) => {
-            Err(err.to_string() + "\n")
-        }
+        Ok(dir) => Ok(dir.into_os_string().into_string().unwrap() + "\n"),
+        Err(err) => Err(err.to_string() + "\n"),
     }
 }
 
@@ -112,28 +131,27 @@ fn handle_cd(args: &[String]) -> Result<String, String> {
     if args[0] == "~" {
         let sys = if cfg!(target_os = "windows") {
             "USERPROFILE"
-        }else {
+        } else {
             "HOME"
         };
-        let path = Path::new(sys);
+        let path_str = env::var(sys).unwrap();
+        let path = Path::new(&path_str);
         return match env::set_current_dir(path) {
             Ok(_) => Ok("".to_string()),
-            Err(err) => {
-                Err(err.to_string())
-            }
-        }
+            Err(err) => Err(err.to_string()),
+        };
     }
     let path = Path::new(args[0].as_str());
     match env::set_current_dir(path) {
         Ok(_) => Ok("".to_string()),
-        Err(err) => {
-            Err(err.to_string())
-        }
+        Err(err) => Err(err.to_string()),
     }
 }
 
-fn parse_args(args: String) -> Result<Vec<String>, String> {
+fn parse_args(args: String) -> Result<(Vec<String>, Vec<RedirectOpt>), String> {
     let mut res = Vec::<String>::new();
+    let mut opts = Vec::<RedirectOpt>::new();
+    let mut opt: Option<RedirectOpt> = None;
     let mut in_single_quote = false;
     let mut in_double_quote = false;
     let mut escape_next = false;
@@ -142,20 +160,20 @@ fn parse_args(args: String) -> Result<Vec<String>, String> {
         if in_single_quote {
             if char == '\'' {
                 in_single_quote = false;
-            }else {
+            } else {
                 item.push(char);
             }
-        }else if in_double_quote {
+        } else if in_double_quote {
             if escape_next {
                 escape_next = false;
                 match char {
-                    '$'|'"'|'\\' => item.push(char),
+                    '$' | '"' | '\\' => item.push(char),
                     _ => {
                         item.push('\\');
                         item.push(char);
                     }
                 }
-            }else if char == '"' {
+            } else if char == '"' {
                 in_single_quote = true;
             } else if char == '\\' {
                 escape_next = true;
@@ -167,14 +185,23 @@ fn parse_args(args: String) -> Result<Vec<String>, String> {
             item.push(char);
         } else {
             match char {
-                '"'=> in_double_quote = true,
-                '\''=> in_single_quote = true,
+                '"' => in_double_quote = true,
+                '\'' => in_single_quote = true,
                 '\\' => escape_next = true,
                 ' ' => {
                     if item.len() != 0 {
-                        res.push(item.clone());
+                        if let Some(mut reOpt) = opt {
+                            reOpt.path = item.clone();
+                            item.clear();
+                            opt = None;
+                            opts.push(reOpt);
+                            continue;
+                        }
+                        opt = is_redirect(item.as_str());
+                        if opt.is_none() {
+                            res.push(item.clone());
+                        }
                         item.clear();
-
                     }
                 }
                 _ => {
@@ -187,9 +214,66 @@ fn parse_args(args: String) -> Result<Vec<String>, String> {
         return Err("unclosed quotes".to_string());
     }
     if item.len() != 0 {
-        res.push(item);
+        if let Some(mut opt) = opt {
+            opt.path = item.clone();
+            opts.push(opt);
+        } else {
+            res.push(item.clone());
+        }
     }
-    Ok(res)
+    Ok((res, opts))
 }
 
+fn is_redirect(flag: &str) -> Option<RedirectOpt> {
+    match flag {
+        ">" | "1>" => Some(RedirectOpt {
+            is_stderr: false,
+            is_append: false,
+            path: "".to_string(),
+        }),
+        ">>" | "1>>" => Some(RedirectOpt {
+            is_stderr: false,
+            is_append: true,
+            path: "".to_string(),
+        }),
+        "2>" => Some(RedirectOpt {
+            is_stderr: true,
+            is_append: false,
+            path: "".to_string(),
+        }),
+        "2>>" => Some(RedirectOpt {
+            is_stderr: true,
+            is_append: true,
+            path: "".to_string(),
+        }),
+        _ => None,
+    }
+}
 
+fn is_executable(flag: &str) -> (bool, String) {
+    // 获取环境变量
+    let paths = env::var("PATH").unwrap();
+    let path_splits = paths.split(system::SPLIT_SYMBOL).collect::<Vec<&str>>();
+    for path in path_splits {
+        let complete_path = format!("{}/{}{}", path, &flag, system::SUFFIX);
+        let file_opt = fs::metadata(&complete_path);
+        if let Ok(file_info) = file_opt {
+            if file_info.is_dir() {
+                continue;
+            }
+            #[cfg(target_os = "windows")]
+            return (true, complete_path.to_string());
+            #[cfg(not(target_os = "windows"))]
+            {
+                let permissions = file_info.permissions();
+                if permissions.mode() & 0o111 == 0 {
+                    continue;
+                }
+                return (true, complete_path.to_string());
+            }
+        } else {
+            continue;
+        }
+    }
+    (false, "".to_string())
+}
